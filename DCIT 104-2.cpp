98e66ecf38e45901b6cfafef214583213a504dc7
LizzyQuansah <102978750+LizzyQuansah@users.noreply.github.com>
2022-08-08 03:12:19 -0800

    Update DCIT 104-2.cpp

--- DCIT 104-2.cpp
+++ DCIT 104-2.cpp
@@ -1,3 +1,5 @@
+//Write a program that produces the average of all even numbers less than a number
+
 #include <iostream>
 
 using namespace std;
@@ -7,12 +9,12 @@ int main()
     int number = 2;
     int sum = 0;
     int average;
-    while(number < 1000)
+    while(number < 500)
     {
         sum += number;
         number += 2;
     }
-    average = sum / (1000 / 2);
+    average = sum / (500 / 2);
     cout << average << endl;
     return 0;
 }
